5790fc9449881cd3bad95f72d85cc4fd42dd445c
Rafael Fernandes da Silva <81975862+Rafael-Orbia@users.noreply.github.com>
2023-06-20 19:20:21 -0300

    Criar model, controller e routes para editora e associar a editora a um livro

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,17 +3,23 @@ import livros from '../models/Livro.js'
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros)
-        })
+        livros.find()
+            .populate('autor')
+            .populate('editora')
+            .exec((err, livros) => {
+                res.status(200).json(livros)
+            })
     }
 
     static listarLivroPorId = (req, res) => {
         const id = req.params.id;
 
-        livros.findById(id, (err, livros) => {
-            if(err) {
-                res.status(400).send({message: `${err.message} - id do livro não localizado`})
+        livros.findById(id)
+            .populate(`autor`, `nome`)
+            .populate(`editora`, `nome`)
+            .exec((err, livros) => {
+            if (err) {
+                res.status(400).send({ message: `${err.message} - id do livro não localizado` })
             } else {
                 res.status(200).send(livros)
             }
@@ -34,11 +40,11 @@ class LivroController {
     static atualizarLivro = (req, res) => {
         const id = req.params.id;
 
-        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
-            if(!err) {
-                res.status(200).send({message: 'Livro atualizado com sucesso'})
+        livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+            if (!err) {
+                res.status(200).send({ message: 'Livro atualizado com sucesso' })
             } else {
-                res.status(500).send({message: err.message})
+                res.status(500).send({ message: err.message })
             }
         })
     }
@@ -47,10 +53,10 @@ class LivroController {
         const id = req.params.id;
 
         livros.findByIdAndDelete(id, (err) => {
-            if(!err) {
-                res.status(200).send({message: 'Livro removido com sucesso'})
+            if (!err) {
+                res.status(200).send({ message: 'Livro removido com sucesso' })
             } else {
-                res.status(500).send({message: err.message})
+                res.status(500).send({ message: err.message })
             }
         })
     }
